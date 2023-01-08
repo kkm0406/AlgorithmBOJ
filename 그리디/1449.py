75dd924ec06ccff84f19983b5ddc87f0d5afe89f
kkm0406 <sdjmc30412@naver.com>
2023-01-08 20:08:03 +0900

    BOJ 구현 S1 N.1449 수리공 항승

--- 그리디/1449.py
+++ 그리디/1449.py
@@ -4,12 +4,14 @@ import sys
 n, l = map(int, sys.stdin.readline().split())
 arr = sorted(list(map(int, sys.stdin.readline().split())))
 cnt = 0
-pos = 0
+pos = 0  # 테이프 현재 위치
 
 for i in range(n):
     if arr[i] < pos:
+        # 물이 새는 위치보다 테이프 현재 위치가 더 크면 
+        # -> 이미 붙인 곳
         continue
-    pos = arr[i] - 0.5 + l
+    pos = arr[i] - 0.5 + l  # 테이프 위치 초기화
     cnt += 1
 
 print(cnt)
