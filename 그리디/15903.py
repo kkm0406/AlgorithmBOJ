e278dc693fa7079c1e1e23590a7f9277df24d899
kkm0406 <sdjmc30412@naver.com>
2023-01-08 20:05:51 +0900

    BOJ 구현 S1 N.15903 카드 합체 놀이

--- 그리디/15903.py
+++ 그리디/15903.py
@@ -5,8 +5,8 @@ n, m = map(int, sys.stdin.readline().split())
 card = list(map(int, sys.stdin.readline().split()))
 
 for i in range(m):
-    card = sorted(card)
-    result = card[0] + card[1]
+    card = sorted(card)  # 매 합체 시 카드 오름차순 정렬
+    result = card[0] + card[1]  # 0번째 1번째 카드 합
     card[0], card[1] = result, result
 
 print(sum(card))
