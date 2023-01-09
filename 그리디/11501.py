61c06902dc43e303726a6760101c140916eb1b61
kkm0406 <sdjmc30412@naver.com>
2023-01-09 17:16:35 +0900

    BOJ 문제 그리디 S2 N.11501 주식

---
+++ 그리디/11501.py
@@ -0,0 +1,16 @@
+# 주식 S2
+import sys
+
+t = int(sys.stdin.readline())
+
+for _ in range(t):
+    n = int(sys.stdin.readline())
+    stock = list(map(int, sys.stdin.readline().split()))
+    profit = 0  # 최대 가격
+    result = 0  # 최종 이익
+    for i in range(n - 1, -1, -1):  # 뒤에서부터 접근
+        if profit < stock[i]:  # 최대 가격보다  오늘 가격이 높으면
+            profit = stock[i]
+        else:  # 현재 수익에 주식 가격을 빼면 이익
+            result += profit - stock[i]
+    print(result)
