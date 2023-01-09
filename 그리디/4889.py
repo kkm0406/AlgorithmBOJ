e6be0713b5772872191867fc9c90528e25ae3a00
kkm0406 <sdjmc30412@naver.com>
2023-01-09 19:32:03 +0900

    BOJ 분류 S1 N.4889 안정적인 문자열

--- 그리디/4889.py
+++ 그리디/4889.py
@@ -17,8 +17,9 @@ while True:
                 # {로 바꿔 스택에 저장
             else:  # 스택 마지막 원소가 {이므로 pop
                 stack.pop()
-        else:  # {만 저장됨 -> {개 개수 절반을 바꾸면 됨
+        else:  # {만 저장됨
             stack.append(text[i])
+    # {개수의 절반만 }로 바꾸면 됨
     cnt += len(stack) // 2
     print(f"{num}. {cnt}")
     num += 1
