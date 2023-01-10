11f446d877617841800b1a3c8770bbd7a41fcd5a
kkm0406 <50093283+kkm0406@users.noreply.github.com>
2023-01-10 18:10:45 +0900

    23.01.07 BOJ 구현 S1 N.16507 어두운 건 무서워

--- 구현/16507.py
+++ 구현/16507.py
@@ -8,7 +8,7 @@ dp = [[0 for i in range(0, c + 1)] for _ in range(r + 1)]
 for i in range(r):
     arr.append(list(map(int, sys.stdin.readline().split())))
 
-# 픽섹 밝기의 누적합 저장
+# 픽섹 밝기의 누적합
 for i in range(1, r + 1):
     for j in range(1, c + 1):
         dp[i][j] = dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + arr[i - 1][j - 1]
