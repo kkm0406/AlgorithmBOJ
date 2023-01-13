9d4e54d0b62ffea2fb6b7c9e5b2af98979690a75
kkm0406 <sdjmc30412@naver.com>
2023-01-13 22:49:35 +0900

    23.01.13 BOJ 다이나믹 프로그래밍 S2 N.11048 이동하기

--- 다이나믹 프로그래밍/11048.py
+++ 다이나믹 프로그래밍/11048.py
@@ -8,5 +8,6 @@ dp = [[0] * (m + 1) for i in range(n + 1)]
 for i in range(1, n + 1):
     for j in range(1, m + 1):
         dp[i][j] = maze[i - 1][j - 1] + max(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1])
+        # 현재 위치의 최댓값은 미로의 값+ 이전에 방문한 최댓값들중 최대값
 
 print(dp[n][m])
