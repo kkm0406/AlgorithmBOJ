369258749add43d7cfe792989b792d39bbb5c9b1
kkm0406 <sdjmc30412@naver.com>
2023-02-11 20:30:50 +0900

    23.02.11 BOJ 구현 G5 N.15686 치킨 배달

--- 구현/15686.py
+++ 구현/15686.py
@@ -7,22 +7,41 @@ n, m = map(int, input().split())
 arr = [list(map(int, input().split())) for i in range(n)]
 home = []
 chicken = []
+visited = [[0] * n for i in range(n)]
+combs = []
 ans = []
+last_visited = []
 for i in range(n):
     for j in range(n):
         if arr[i][j] == 1:
-            home.append([i, j])  # 가정집 위치
+            home.append([i, j])
         elif arr[i][j] == 2:
-            chicken.append([i, j])  # 치킨집 위치
+            chicken.append([i, j])
+            visited.append(False)
 
-for comb in combinations(chicken, m):  # 조합을 이용해 m개의 치킨집 위치 구함
-    result = 0
-    for hx, hy in home:
-        dist = 2 * n
-        for i in range(m):
-            # 가정집에서 가장 가까운 치킨집위치 초기화
-            dist = min(dist, abs(hx - comb[i][0]) + abs(hy - comb[i][1]))
-        result += dist  # 각 조합에서 치킨거리
-    ans.append(result)
 
-print(min(ans))  # 치킨거리의 최솟값 출력
+def dfs(depth):
+    if depth == m:
+        result = 0
+        for hx, hy in home:  # 모든 집과
+            dist = 2 * n
+            for idx, (cx, cy) in last_visited:  # 뽑은 치킨집과의 거리 구함
+                dist = min(dist, abs(hx - cx) + abs(hy - cy))
+            result += dist
+        ans.append(result)
+        return
+
+    for e, (cx, cy) in enumerate(chicken):  # eumerate로 인덱스, 원소 접근
+        if visited[cx][cy] == 0:  # 치킨집 방문안했으면
+            if last_visited:  # 시간 단축과 오름차순위해
+                if e < last_visited[-1][0]:  # 마지막에 방문한 인덱스가 현재 인덱스보다 작으면 continue
+                    continue
+            visited[cx][cy] = 1  # 치킨집 방문 처리
+            last_visited.append((e, (cx, cy)))
+            dfs(depth + 1)
+            visited[cx][cy] = 0  # 미방문 처리
+            last_visited.pop()
+
+
+dfs(0)
+print(min(ans))
