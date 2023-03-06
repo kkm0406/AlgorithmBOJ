d4c4d01ada96f0bff13a3b597f3d49b9cda7ce22
kkm0406 <sdjmc30412@naver.com>
2023-03-06 12:45:35 +0900

    23.03.06 BOJ 이분 탐색 S1 N.2343 기타 레슨

--- 이분 탐색/2343.py
+++ 이분 탐색/2343.py
@@ -1,31 +1,28 @@
-# 기타 레슨 S1
 import sys
 
 input = sys.stdin.readline
+
 n, m = map(int, input().split())
+
 arr = list(map(int, input().split()))
+l = max(arr)  # 블루레이 개수가 n개일 때 -> arr 최대값
+r = sum(arr)  # 블루레이 개수가 1개일 때 -> arr의 합
+ans = 1e9
+while l <= r:
+    mid = (l + r) // 2
+    cnt = 0
+    sum = 0  # 현재 블루레이 크기
+    for i in range(len(arr)):
+        if sum + arr[i] > mid:  # i번째 추가 시 mid보다 크면
+            cnt += 1  # 분할
+            sum = 0  # 초기화
+        sum += arr[i]
+    if sum:
+        cnt += 1
 
-start = max(arr)  # 최소 블루레이의 크기 ( n==m일 때 -> max(arr))
-end = sum(arr)  # 최대 블루레이의 크기 (m==1일 때)
-result = sum(arr)
-while start <= end:
-    mid = (start + end) // 2  # 블루레이의 크기
-    if mid < max(arr):  # 블루레이 크기가 전체 합보다 작으면
-        start = mid + 1
-        continue
-    cnt = 1  # 블루레이 개수
-    blue_ray = 0  # 블루레이 크기
-    for i in arr:
-        if blue_ray + i <= mid:  # i를 추가했을 때 mid를 넘지 않으면
-            blue_ray += i
-        else:  # i추가 시 mid를 초과하면
-            cnt += 1  # 블루레이 개수 +1
-            blue_ray = i
-
-    if cnt <= m:  # 블루레이 개수가 m보다 작으면
-        end = mid - 1
-        result = min(result, mid)
+    if cnt > m:  # 블루레이 개수가 m보다 큰 경우 -> 각 블루레이 크기가 작음
+        l = mid + 1
     else:
-        start = mid + 1
-
-print(result)
+        ans = min(ans, mid)
+        r = mid - 1
+print(ans)
