ada570fd8aea27a33f3c2f7c63794356b0aae6ac
kkm0406 <sdjmc30412@naver.com>
2023-03-07 19:01:14 +0900

    23.03.07 BOJ 이분 탐색 S2 N.16401 과자 나눠주기

--- 이분 탐색/16401.py
+++ 이분 탐색/16401.py
@@ -6,14 +6,16 @@ m, n = map(int, input().split())
 
 arr = list(map(int, input().split()))
 
-start = 1
-end = max(arr)
+start = 1  # zero division error -> 1로 설정
+end = max(arr)  # 과자 최댓값
 ans = 0
 while start <= end:
-    mid = (start + end) // 2
+    mid = (start + end) // 2  # 나누어줄 과자 크기
     cnt = 0
     for i in arr:
-        cnt += i // mid
+        cnt += i // mid  # 나누어줄 수 있는 과자 개수 연산
+
+    # 나누어줄 수 있는 과자 개수와 조카 수 비교하면서 이분탐색 진행
     if cnt < m:
         end = mid - 1
     else:
