bb2cbb119601aafdafa849c403497e0f8beb7681
kkm0406 <sdjmc30412@naver.com>
2023-03-07 18:59:22 +0900

    23.03.07 BOJ 다이나믹 프로그래밍 S1 N.2502 떡 먹는 호랑이

--- 다이나믹 프로그래밍/2502.py
+++ 다이나믹 프로그래밍/2502.py
@@ -4,15 +4,17 @@ import sys
 input = sys.stdin.readline
 d, k = map(int, input().split())
 
-dp = [[0, 0] for i in range(d + 1)]
+dp = [[0, 0] for i in range(d + 1)]  # dp[n] -> day1, day2 몇개로 이루어졌는지
 dp[1] = [1, 0]
 dp[2] = [0, 1]
-dp[3] = [1, 1]
+dp[3] = [1, 1]  # ex) dp[3] = dp[1]+dp[2], dp[4] = dp[3]+dp[2] = dp[2] +dp[1] + dp[1]
 for i in range(4, d + 1):
     dp[i] = [dp[i - 1][0] + dp[i - 2][0], dp[i - 1][1] + dp[i - 2][1]]
 
-x, y = dp[-1][0], dp[-1][1]
-a = 1
+x, y = dp[-1][0], dp[-1][1]  # 최종 day1, day2 개수
+a = 1  # day1준 떡의 개수
+# day1개수 * day1 떡의 개수 + day2개수 * day2 떡의 개수 = k
+# -> day1 떡의 개수, day2 떡의 개수 찾기
 while True:
     result = k - (x * a)
     if result % y == 0:
