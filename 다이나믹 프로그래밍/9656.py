85c2a430ee2cd5bbd9719010f4a9218ee6c77a1e
kkm0406 <sdjmc30412@naver.com>
2023-03-31 17:35:21 +0900

    23.03.31 BOJ 다이나믹 프로그래밍 S4 N.9656 돌 게임 2

--- 다이나믹 프로그래밍/9656.py
+++ 다이나믹 프로그래밍/9656.py
@@ -8,3 +8,19 @@ if n % 2 == 0:
     print('SK')
 else:
     print('CY')
+
+# ----------------------------------------
+
+# 마지막에 돌을 누가 가져가는지 -> 1이면 상근, 0이면 창영
+dp = [0 for i in range(1001)]
+dp[1], dp[2], dp[3], dp[4] = 1, 0, 1, 0
+
+for i in range(5, n + 1):
+    # 1개 또는 3개를 가져갈 수 있으므로 상대방의 선택지는 i-1 or i-3
+    if not dp[i - 1] and not dp[i - 3]:
+        dp[i] = 1
+
+if dp[n]:
+    print('CY')
+else:
+    print('SK')
