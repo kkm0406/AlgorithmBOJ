9de24a14b7465150e34d38d4b7cce0fb7057dfc2
kkm0406 <sdjmc30412@naver.com>
2023-04-01 20:52:26 +0900

    23.04.01 BOJ 구현 S3 N.2503 숫자 야구

--- 구현/2503.py
+++ 구현/2503.py
@@ -14,16 +14,16 @@ for _ in range(n):
     idx = 0
     for i in range(len(perms)):
         i -= idx
-        cnt_s, cnt_b = 0, 0 #스트라이크 수, 볼 수
+        cnt_s, cnt_b = 0, 0  # 스트라이크 수, 볼 수
         for j in range(3):
             num[j] = int(num[j])
-            if num[j] in perms[i]: #입력한 숫자의 j번째 원소가 순열의 i번째 원소에 있으면
-                if j == perms[i].index(num[j]): #있는 위치의 인덱스가 같으면
-                    cnt_s += 1 #스트라이크
+            if num[j] in perms[i]:  # 입력한 숫자의 j번째 원소가 순열의 i번째 원소에 있으면
+                if j == perms[i].index(num[j]):  # 있는 위치의 인덱스가 같으면
+                    cnt_s += 1  # 스트라이크
                 else:
-                    cnt_b += 1 #볼
-        if cnt_b != b or cnt_s != s: #스트라이크, 볼 수에 맞지 않으면
-            perms.remove(perms[i]) #제거
-            idx += 1 #제거했으니
+                    cnt_b += 1  # 볼
+        if cnt_b != b or cnt_s != s:  # 스트라이크, 볼 수에 맞지 않으면
+            perms.remove(perms[i])  # 제거
+            idx += 1  # 제거했으니
 
 print(len(perms))
