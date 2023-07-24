3f49d261ea76e95bcd0723e7f76d71f4731de4f6
kkm0406 <sdjmc30412@naver.com>
2023-07-24 22:22:17 +0900

    23.07.24 BOJ 다이나믹 프로그래밍 S4 N.2491 수열

---
+++ 다이나믹 프로그래밍/2491.py
@@ -0,0 +1,14 @@
+# 수열 S4
+
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+arr = list(map(int, input().split()))
+dp1, dp2 = [1] * n, [1] * n
+for i in range(1, n):
+    if arr[i] <= arr[i - 1]:
+        dp1[i] = max(dp1[i], dp1[i - 1] + 1)
+    if arr[i] >= arr[i - 1]:
+        dp2[i] = max(dp2[i], dp2[i - 1] + 1)
+print(max(max(dp1), max(dp2)))
